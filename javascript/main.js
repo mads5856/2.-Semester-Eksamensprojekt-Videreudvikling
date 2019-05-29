c4f1195afdb17cb6b6a58733205c9162c02be2f1
mads5856 <mads.normann@jubii.dk>
2019-05-29 12:33:21 +0200

    Interaktivt map fulført

--- javascript/main.js
+++ javascript/main.js
@@ -105,12 +105,23 @@ function profil1Forside() {
 }
 
 
-function profil2() {    
-    document.getElementById("infoOverlay").style.display = "block";
+function profil2Forside() {    
+    document.getElementById("forsideOverlay").style.display = "block";
     document.getElementById("navn").innerHTML = "Navn:" + " " + medarbejderInfo[1][0];
     document.getElementById("rang").innerHTML = "Rang:" + " " + medarbejderInfo[1][1];
     document.getElementById("year").innerHTML = "Pensioneret:" + " " + medarbejderInfo[1][2];
     document.getElementById("treeNum").innerHTML = "Træ nr.:" + " " + medarbejderInfo[1][3];
     document.getElementById("smallBox").innerHTML = "Her skal der f.eks. stå en kort historie om" + " " + medarbejderInfo[1][0] + ", " + "eller et citat fra ham.";
     document.getElementById("largeBox").innerHTML = "Her skal der stå mere information om" + " " + medarbejderInfo[1][0] + ", " + "og deres tid og bedrifter i regimentet.";
+}
+
+
+function profil3Forside() {    
+    document.getElementById("forsideOverlay").style.display = "block";
+    document.getElementById("navn").innerHTML = "Navn:" + " " + medarbejderInfo[2][0];
+    document.getElementById("rang").innerHTML = "Rang:" + " " + medarbejderInfo[2][1];
+    document.getElementById("year").innerHTML = "Pensioneret:" + " " + medarbejderInfo[2][2];
+    document.getElementById("treeNum").innerHTML = "Træ nr.:" + " " + medarbejderInfo[2][3];
+    document.getElementById("smallBox").innerHTML = "Her skal der f.eks. stå en kort historie om" + " " + medarbejderInfo[2][0] + ", " + "eller et citat fra ham.";
+    document.getElementById("largeBox").innerHTML = "Her skal der stå mere information om" + " " + medarbejderInfo[2][0] + ", " + "og deres tid og bedrifter i regimentet.";
 }
