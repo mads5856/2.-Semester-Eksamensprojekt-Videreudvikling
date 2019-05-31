4ce894ae6bab566ab328cbfcfe84f81be06331ce
mads5856 <mads.normann@jubii.dk>
2019-05-31 13:10:43 +0200

    Sorterings filter

--- javascript/main.js
+++ javascript/main.js
@@ -62,7 +62,7 @@ function profil3() {
 
 
 
-//Kode til soteringsknappen
+//Kode til sorteringsknappen
 
 function sortOverlay() {
   let x = document.getElementById("sorterOverlay");
@@ -172,4 +172,167 @@ function treeHighlight() {
     document.getElementById("tree2").style.fill = "red";
     document.getElementById("tree1").style.fill = "5F984C";
     }
+}
+
+
+//Kode til sortéring
+
+let sortMove = [
+    ['Navn: E. Frandsen', 'Rang: MJ', 'Pensioneret: 30. mar. 1983'],
+    
+    ['Navn: S.AA Mathiesen', 'Rang: KTBTJ', 'Pensioneret: 30. mar. 1983'],
+    
+    ['Navn: E. Knudsen', 'Rang: MJ', 'Pensioneret: 30. jun. 1983'],
+    
+    ['Navn: S.AA. Kristensen', 'Rang: KN', 'Pensioneret: 30. okt. 1983'],
+    
+    ['Navn: E. Mogensen', 'Rang: OL', 'Pensioneret: 31. aug. 1984'],
+    
+    ['Navn: E.F. Nielsen', 'Rang: MJ', 'Pensioneret: 31. aug. 1984'],
+    
+    ['Navn: K.AA. Christiansen', 'Rang: KN', 'Pensioneret: 31. dec. 1984'],
+    
+    ['Navn: E.V. Tomsen', 'Rang: DEPARB', 'Pensioneret: 28. feb. 1985'],
+    
+    ['Navn: S.A.M. Nielsen', 'Rang: MJ', 'Pensioneret: 30. sep. 1985'],
+    
+    ['Navn: O.E. Jacobsen', 'Rang: KN', 'Pensioneret: 31. maj. 1986'],
+    
+    ['Navn: H.L.N. Olesen', 'Rang: MJ', 'Pensioneret: 30. apr. 1987'],
+    
+    ['Navn: R. Kappel-Hansen', 'Rang: KN', 'Pensioneret: 31. aug. 1987'],
+];
+
+
+let sortBtn = ['Årstal', 'Navn', 'Rang', 'Træ nr.']
+
+
+document.getElementById("sorterBtn").innerHTML = "Sortér efter: " + sortBtn[0];
+
+document.getElementById("sorterNavn").innerHTML = sortBtn[1]
+
+document.getElementById("sorterRang").innerHTML = sortBtn[2]
+
+document.getElementById("sorterAarstal").innerHTML = sortBtn[0]
+
+
+document.getElementById("boks1").innerHTML = sortMove[0][0] + "<br>" + sortMove[0][1] + "<br>" + sortMove[0][2];
+
+document.getElementById("boks2").innerHTML = sortMove[1][0] + "<br>" + sortMove[1][1] + "<br>" + sortMove[1][2];
+
+document.getElementById("boks3").innerHTML = sortMove[2][0] + "<br>" + sortMove[2][1] + "<br>" + sortMove[2][2];
+
+document.getElementById("boks4").innerHTML = sortMove[3][0] + "<br>" + sortMove[3][1] + "<br>" + sortMove[3][2];
+
+document.getElementById("boks5").innerHTML = sortMove[4][0] + "<br>" + sortMove[4][1] + "<br>" + sortMove[4][2];
+
+document.getElementById("boks6").innerHTML = sortMove[5][0] + "<br>" + sortMove[5][1] + "<br>" + sortMove[5][2];
+
+document.getElementById("boks7").innerHTML = sortMove[6][0] + "<br>" + sortMove[6][1] + "<br>" + sortMove[6][2];
+
+document.getElementById("boks8").innerHTML = sortMove[7][0] + "<br>" + sortMove[7][1] + "<br>" + sortMove[7][2];
+
+document.getElementById("boks9").innerHTML = sortMove[8][0] + "<br>" + sortMove[8][1] + "<br>" + sortMove[8][2];
+
+document.getElementById("boks10").innerHTML = sortMove[9][0] + "<br>" + sortMove[9][1] + "<br>" + sortMove[9][2];
+
+document.getElementById("boks11").innerHTML = sortMove[10][0] + "<br>" + sortMove[10][1] + "<br>" + sortMove[10][2];
+
+document.getElementById("boks12").innerHTML = sortMove[11][0] + "<br>" + sortMove[11][1] + "<br>" + sortMove[11][2];
+
+
+
+
+function sortNavn() {
+    document.getElementById("sorterBtn").innerHTML = "Sortér efter: " + sortBtn[1];
+    
+    
+    document.getElementById("boks1").innerHTML = sortMove[6][0] + "<br>" + sortMove[6][1] + "<br>" + sortMove[6][2];
+    
+    document.getElementById("boks2").innerHTML = sortMove[0][0] + "<br>" + sortMove[0][1] + "<br>" + sortMove[0][2];
+    
+    document.getElementById("boks3").innerHTML = sortMove[9][0] + "<br>" + sortMove[9][1] + "<br>" + sortMove[9][2];
+    
+    document.getElementById("boks4").innerHTML = sortMove[11][0] + "<br>" + sortMove[11][1] + "<br>" + sortMove[11][2];
+    
+    document.getElementById("boks5").innerHTML = sortMove[2][0] + "<br>" + sortMove[2][1] + "<br>" + sortMove[2][2];
+    
+    document.getElementById("boks6").innerHTML = sortMove[3][0] + "<br>" + sortMove[3][1] + "<br>" + sortMove[3][2];
+    
+    document.getElementById("boks7").innerHTML = sortMove[1][0] + "<br>" + sortMove[1][1] + "<br>" + sortMove[1][2];
+    
+    document.getElementById("boks8").innerHTML = sortMove[4][0] + "<br>" + sortMove[4][1] + "<br>" + sortMove[4][2];
+    
+    document.getElementById("boks9").innerHTML = sortMove[5][0] + "<br>" + sortMove[5][1] + "<br>" + sortMove[5][2];
+    
+    document.getElementById("boks10").innerHTML = sortMove[8][0] + "<br>" + sortMove[8][1] + "<br>" + sortMove[8][2];
+    
+    document.getElementById("boks11").innerHTML = sortMove[10][0] + "<br>" + sortMove[10][1] + "<br>" + sortMove[10][2];
+    
+    document.getElementById("boks12").innerHTML = sortMove[7][0] + "<br>" + sortMove[7][1] + "<br>" + sortMove[7][2];
+}
+
+
+function sortRang() {
+    document.getElementById("sorterBtn").innerHTML = "Sortér efter: " + sortBtn[2];
+    
+    document.getElementById("boks1").innerHTML = sortMove[7][0] + "<br>" + sortMove[7][1] + "<br>" + sortMove[7][2];
+    
+    document.getElementById("boks2").innerHTML = sortMove[6][0] + "<br>" + sortMove[6][1] + "<br>" + sortMove[6][2];
+    
+    document.getElementById("boks3").innerHTML = sortMove[9][0] + "<br>" + sortMove[9][1] + "<br>" + sortMove[9][2];
+    
+    document.getElementById("boks4").innerHTML = sortMove[11][0] + "<br>" + sortMove[11][1] + "<br>" + sortMove[11][2];
+    
+    document.getElementById("boks5").innerHTML = sortMove[3][0] + "<br>" + sortMove[3][1] + "<br>" + sortMove[3][2];
+    
+    document.getElementById("boks6").innerHTML = sortMove[1][0] + "<br>" + sortMove[1][1] + "<br>" + sortMove[1][2];
+    
+    document.getElementById("boks7").innerHTML = sortMove[0][0] + "<br>" + sortMove[0][1] + "<br>" + sortMove[0][2];
+    
+    document.getElementById("boks8").innerHTML = sortMove[2][0] + "<br>" + sortMove[2][1] + "<br>" + sortMove[2][2];
+    
+    document.getElementById("boks9").innerHTML = sortMove[5][0] + "<br>" + sortMove[5][1] + "<br>" + sortMove[5][2];
+    
+    document.getElementById("boks10").innerHTML = sortMove[8][0] + "<br>" + sortMove[8][1] + "<br>" + sortMove[8][2];
+    
+    document.getElementById("boks11").innerHTML = sortMove[10][0] + "<br>" + sortMove[10][1] + "<br>" + sortMove[10][2];
+    
+    document.getElementById("boks12").innerHTML = sortMove[4][0] + "<br>" + sortMove[4][1] + "<br>" + sortMove[4][2];
+}
+
+
+function sortAarstal() {
+
+document.getElementById("sorterBtn").innerHTML = "Sortér efter: " + sortBtn[0];
+
+document.getElementById("sorterNavn").innerHTML = sortBtn[1]
+
+document.getElementById("sorterRang").innerHTML = sortBtn[2]
+
+document.getElementById("sorterAarstal").innerHTML = sortBtn[0]
+
+document.getElementById("boks1").innerHTML = sortMove[0][0] + "<br>" + sortMove[0][1] + "<br>" + sortMove[0][2];
+
+document.getElementById("boks2").innerHTML = sortMove[1][0] + "<br>" + sortMove[1][1] + "<br>" + sortMove[1][2];
+
+document.getElementById("boks3").innerHTML = sortMove[2][0] + "<br>" + sortMove[2][1] + "<br>" + sortMove[2][2];
+
+document.getElementById("boks4").innerHTML = sortMove[3][0] + "<br>" + sortMove[3][1] + "<br>" + sortMove[3][2];
+
+document.getElementById("boks5").innerHTML = sortMove[4][0] + "<br>" + sortMove[4][1] + "<br>" + sortMove[4][2];
+
+document.getElementById("boks6").innerHTML = sortMove[5][0] + "<br>" + sortMove[5][1] + "<br>" + sortMove[5][2];
+
+document.getElementById("boks7").innerHTML = sortMove[6][0] + "<br>" + sortMove[6][1] + "<br>" + sortMove[6][2];
+
+document.getElementById("boks8").innerHTML = sortMove[7][0] + "<br>" + sortMove[7][1] + "<br>" + sortMove[7][2];
+
+document.getElementById("boks9").innerHTML = sortMove[8][0] + "<br>" + sortMove[8][1] + "<br>" + sortMove[8][2];
+
+document.getElementById("boks10").innerHTML = sortMove[9][0] + "<br>" + sortMove[9][1] + "<br>" + sortMove[9][2];
+
+document.getElementById("boks11").innerHTML = sortMove[10][0] + "<br>" + sortMove[10][1] + "<br>" + sortMove[10][2];
+
+document.getElementById("boks12").innerHTML = sortMove[11][0] + "<br>" + sortMove[11][1] + "<br>" + sortMove[11][2];
 }
